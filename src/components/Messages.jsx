7aac75a4608d8138034ed69aa3b0700a374094bd
Tobias Wilker <twilker@phoenixcontact.com>
2022-04-23 05:52:05 +0200

    Prettify

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -5,12 +5,21 @@ export default function Messages({ messages }) {
   return (
     <>
       <h2>Messages</h2>
-      {messages.map((message, i) =>
+      {messages.sort((a,b) => b.timestamp-a.timestamp).map((message, i) =>
         // TODO: format as cards, add timestamp
-        <p key={i} className={message.premium ? 'is-premium' : ''}>
-          <strong>{message.sender}</strong>:<br/>
-          {message.text}
-        </p>
+        <div className="row" key={i}>
+            <div className={message.premium ? 'card horizontal blue white-text' : 'card horizontal'}>
+                <div className="card-stacked">
+                    <div className="card-title">{message.sender}</div>
+                    <div className="card-content">
+                      <p>{message.text}</p>
+                    </div>
+                    <div className={message.premium ? 'card-action border-white' : 'card-action'}>
+                      {new Date(message.timestamp/1000000).toLocaleString()}
+                    </div>
+                </div>
+            </div>
+        </div>
       )}
     </>
   );
